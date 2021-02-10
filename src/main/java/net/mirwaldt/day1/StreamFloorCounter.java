dcfbe96fe8a34823891b06d866f90b1044efeb4a
mmirwaldt <michael.mirwaldt@gmail.com>
2021-02-10 01:51:03 +0100

    layout improved

--- src/main/java/net/mirwaldt/day1/StreamFloorCounter.java
+++ src/main/java/net/mirwaldt/day1/StreamFloorCounter.java
@@ -3,6 +3,6 @@ package net.mirwaldt.day1;
 public class StreamFloorCounter implements FloorCounter {
     @Override
     public int countFloors(String input) {
-        return input.chars().map(aChar -> ((aChar=='(')?1:-1)).sum();
+        return input.chars().map(aChar -> ((aChar == '(') ? 1 : -1)).sum();
     }
 }
